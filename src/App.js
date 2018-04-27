27f3658037b45e195b9bfbd84659f2c96d1387b6
ddenis <dhffdh@gmail.com>
2018-04-27 13:56:24 +0800

    Fixes v6

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component, PureComponent}  from 'react';
+import React, {Component}  from 'react';
 import './App.scss';
 import { YMaps, Map, Polyline, GeoObject } from 'react-yandex-maps';
 
